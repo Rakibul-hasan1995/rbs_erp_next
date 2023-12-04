651bd5784a6ded1677692e2539a7b7b8544294b3
rakib <rakibulhasan.bdm@gmail.com>
2023-12-04 18:26:33 +0600

    sdf

--- src/app/v1/layout.tsx
+++ src/app/v1/layout.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import UiLayout from './ui_layout';
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation'
-import { authOptions } from '../api/auth/[...nextauth]/authOptions';
 export default async function RootLayout({
    children,
 }: {
    children: React.ReactNode
 }) {
-   const session = await getServerSession(authOptions)
-   if (!session) {
-      redirect('/auth/signin')
-   }
+   // const session = await getServerSession(authOptions)
+   // if (!session) {
+   //    redirect('/auth/signin')
+   // }
 
    return (
       <UiLayout>
